30a93b8b9b91e2566a72f0dde4d8359f41942ce7
medmarwane <medmarwane@gmail.com>
2022-07-18 16:19:35 +0200

    Creating first heroBanner ang display it in the frontend

--- compare.js
+++ compare.js
@@ -2,9 +2,9 @@ import sanityClient from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url"
 
 export const client = sanityClient({
-  projectId: "5vwskr0s",
+  projectId: "vfxfwnaw",
   dataset: "production",
-  apiVersion: "2022-07-18",
+  apiVersion: "2022-03-10",
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
